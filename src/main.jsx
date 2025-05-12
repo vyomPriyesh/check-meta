b55b9d84fc9cb9733cd97840ac99a32250f3e4f2
vyomPriyesh <vyompriyesh@gmail.com>
2025-05-12 10:53:30 +0530

    Add

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+// src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.hydrateRoot(
+  document.getElementById('root'),
+    <App />
+)
